5c2532992845c40aae4221c22b35674d42b543d8
Melissa Evelyn da Silva Lima Matos <melissaeslmatos@gmail.com>
2026-08-09 16:40:54 -0300

    ajustes de servidor

--- dashboard-financeiro/src/app/features/features/dashboard/dashboard.ts
+++ dashboard-financeiro/src/app/features/features/dashboard/dashboard.ts
@@ -1,12 +1,13 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import { PieChartComponent } from '../../../shared/charts/pie-chart/pie-chart';
 import { TransactionStore } from '../../../data-access/transactions/transation.store';
 import { BarChartComponent } from '../../../shared/charts/bar-chart/bar-chart';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CurrencyPipe, PieChartComponent, BarChartComponent, DatePipe],
+  imports: [CurrencyPipe, PieChartComponent, BarChartComponent, DatePipe, RouterLink],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
@@ -22,4 +23,6 @@ export class DashboardComponent {
   monthlyEvolution = this.store.monthlyEvolution;
   recentTransactions = this.store.recentTransactions;
   transactionCount = this.store.transactionCount;
+
+  hasTransactions = computed(() => this.store.transactions().length > 0);
 }
--- dashboard-financeiro/src/main.ts
+++ dashboard-financeiro/src/main.ts
@@ -1,10 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { isDevMode } from '@angular/core';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app';
 
 async function enableMocking() {
-  if (!isDevMode()) return;
   const { worker } = await import('./app/mocks/browser');
   return worker.start();
 }
