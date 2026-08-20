81aacf595a01acab20bf52133213b33f957c8562
Melissa Evelyn da Silva Lima Matos <melissaeslmatos@gmail.com>
2026-08-20 20:41:03 -0300

    bloqueando login do usuario logado

--- dashboard-financeiro/src/app/app.routes.ts
+++ dashboard-financeiro/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth-guard';
+import { guestGuard } from './core/guards/guest-guard';
 
 export const routes: Routes = [
     {
@@ -9,6 +10,7 @@ export const routes: Routes = [
   },
    {
     path: 'login',
+    canActivate: [guestGuard],
     loadComponent: () => import('./features/auth/login/login')
       .then(m => m.LoginComponent)
   },
